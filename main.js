8a1ab352568842d418178aed3224cbe5c4d06bf2
kyledobash <kyledobash@yahoo.com>
2021-04-30 11:41:17 -0700

    added an ajax get request

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-$(function)
+$(function() {
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://pokeapi.co/api/v2/pokemon/' + pokeSearchInput,
+        success: function(res) {
+            console.log(res);
+            alert(res);
+        }
+    });
+});
